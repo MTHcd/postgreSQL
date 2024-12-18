b21a1f0813fd04285d194464b13efca183935b88
MTHcd <mathm2551@gmail.com>
2024-12-18 16:05:02 +0100

    Create codewars.sql

---
+++ codewars.sql
@@ -0,0 +1,25 @@
+-- Students who excel in Science over Math
+
+-- SQL Bug Fixing: Fix the QUERY - Totaling
+
+-- SQL Statistics: MIN, MEDIAN, MAX
+
+-- Present XML data the SQL way
+
+-- Easy SQL: Moving Values
+
+-- SQL: Regex Replace
+
+-- Vowel Count
+
+-- SQL Basics: Create a FUNCTION
+
+-- Reduce My Fraction
+
+-- Disemvowel Trolls
+
+-- BASICS: Length based SELECT with LIKE
+
+-- SQL Basics: Simple JOIN
+
+-- Sum of odd numbers 
